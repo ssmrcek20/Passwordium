bfdcadb71248d71604f033619a86cc0af552793e
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-08 18:49:37 +0200

    Napravljen dizajn za Registracija.form

---
+++ src/Registracija.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Registracija {
+    private JPanel panRegistracija;
+    private JTextField txtKorIme;
+    private JButton btnRegistracija;
+    private JPasswordField txtLozinka;
+    public Registracija() {
+        btnRegistracija.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
