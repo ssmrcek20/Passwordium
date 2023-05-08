6e0b90246dbfec63268f030bbfc79b49cec496a2
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-08 18:33:01 +0200

    Dodani gumbi za prijavu i otvranje forme za registraciju

--- src/Prijava.java
+++ src/Prijava.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Prijava extends JFrame {
     private JPanel panPrijava;
     private JTextField txtKorIme;
-    private JTextField txtLozinka;
     private JButton btnPrijava;
     private JButton btnRegistracija;
-
+    private JPasswordField txtLozinka;
 
 
     public Prijava(){
@@ -21,12 +22,25 @@ public class Prijava extends JFrame {
         btnPrijava.setBorderPainted(false);
         btnPrijava.setBackground(new Color(200,200,200));
         btnPrijava.setFocusPainted(false);
+        btnPrijava.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
         btnRegistracija.setContentAreaFilled(false);
         btnRegistracija.setBorderPainted(false);
         btnRegistracija.setOpaque(false);
         btnRegistracija.setFocusPainted(false);
         btnRegistracija.setForeground(Color.blue);
+        btnRegistracija.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
     }
 
 }
