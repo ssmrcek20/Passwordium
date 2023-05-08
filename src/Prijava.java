3b06692512606ac5e598030f9979374f988f78a5
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-08 14:52:18 +0200

    Kreiranje Main funkcije i prijava forme

--- src/Prijava.java
+++ src/Prijava.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Prijava {
+import javax.swing.*;
+
+public class Prijava extends JFrame {
+    public Prijava(){
+        setTitle("Passwordium");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300,200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
