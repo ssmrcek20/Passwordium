beda966015e2be9c2c60dac0312802ef73423cb8
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-08 17:22:59 +0200

    Dovršen dizajn za Prijava.form

--- src/Prijava.java
+++ src/Prijava.java
@@ -1,11 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Prijava extends JFrame {
+    private JPanel panPrijava;
+    private JTextField txtKorIme;
+    private JTextField txtLozinka;
+    private JButton btnPrijava;
+    private JButton btnRegistracija;
+
+
+
     public Prijava(){
         setTitle("Passwordium");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300,200);
+        setSize(1080,720);
         setLocationRelativeTo(null);
         setVisible(true);
+        setContentPane(panPrijava);
+
+        btnPrijava.setBorderPainted(false);
+        btnPrijava.setBackground(new Color(200,200,200));
+        btnPrijava.setFocusPainted(false);
+
+        btnRegistracija.setContentAreaFilled(false);
+        btnRegistracija.setBorderPainted(false);
+        btnRegistracija.setOpaque(false);
+        btnRegistracija.setFocusPainted(false);
+        btnRegistracija.setForeground(Color.blue);
     }
+
 }
