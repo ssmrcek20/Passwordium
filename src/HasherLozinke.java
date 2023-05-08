244acf41becb8f2eedc8b120e58d29bffd6cf228
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-08 23:27:25 +0200

    Dodane funkcije za dohvaćanje hash datoteke i provjere da li postoji hash datoteka

--- src/HasherLozinke.java
+++ src/HasherLozinke.java
@@ -1,14 +1,11 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HasherLozinke {
-    public String hash(String korIme, String lozinka) throws NoSuchAlgorithmException{
+    public String napraviHash(String korIme, String lozinka) throws NoSuchAlgorithmException{
         String salt = korIme + "AJCKECHJEKJSJDSJRTZH";
 
         MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -22,14 +19,25 @@ public class HasherLozinke {
         return sb.toString();
     }
 
-    public void spremiKorImeILozinku(String korIme, String hashedLozinka) throws IOException{
-        File datoteka = new File(korIme + ".txt");
-        if(datoteka.exists()){
+    public void spremiLozinku(String korIme, String hashedLozinka) throws IOException{
+        if(postojiHashDatoteka(korIme)){
            throw new FileAlreadyExistsException("To korisničko ime se već koristi!");
         } else {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(datoteka))) {
-                writer.write(korIme + ":" + hashedLozinka);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(korIme + ".txt"))) {
+                writer.write(hashedLozinka);
             }
         }
+    }
+
+    public boolean postojiHashDatoteka(String korIme){
+        File datoteka = new File(korIme + ".txt");
+        return datoteka.exists();
+    }
+
+    public String dohvatiHash(String korIme)throws IOException{
+        try (BufferedReader reader = new BufferedReader(new FileReader(korIme + ".txt"))) {
+            return reader.readLine();
+        }
+
     }
 }
