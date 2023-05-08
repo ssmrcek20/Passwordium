ad2675be0e16e85699a2cc0a3df5d6db84560c3b
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-08 21:31:38 +0200

    Registracija korisnika prilikom pritiska na gumb za registraciju te provjera je li lozinka i korisničko ime upisano

--- src/Registracija.java
+++ src/Registracija.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class Registracija extends JFrame {
     private JPanel panRegistracija;
@@ -22,7 +26,30 @@ public class Registracija extends JFrame {
         btnRegistracija.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if(txtKorIme.getText().equals("")){
+                    txtKorIme.setBackground(Color.red);
+                } else if (txtLozinka.getPassword().length == 0) {
+                    txtLozinka.setBackground(Color.red);
+                    txtKorIme.setBackground(Color.white);
+                } else {
+                    txtLozinka.setBackground(Color.white);
+                    HasherLozinke hasherLozinke= new HasherLozinke();
+                    String hashedLozinka = null;
+                    try {
+                        hashedLozinka = hasherLozinke.hash(txtKorIme.getText(),new String(txtLozinka.getPassword()));
+                    } catch (NoSuchAlgorithmException ex) {
+                        JOptionPane.showMessageDialog(Registracija.this,"Ne postoji SHA-256 na računalu!");
+                    }
+                    try {
+                        hasherLozinke.spremiKorImeILozinku(txtKorIme.getText(),hashedLozinka);
+                        JOptionPane.showMessageDialog(Registracija.this,"Uspješna registarcija!");
+                        Registracija.this.dispose();
+                    } catch (FileAlreadyExistsException ex){
+                        JOptionPane.showMessageDialog(Registracija.this,ex.getMessage());
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(Registracija.this,"Došlo je do greške tijekom upisa u datoteku!");
+                    }
+                }
             }
         });
     }
