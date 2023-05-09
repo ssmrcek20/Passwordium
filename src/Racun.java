9570c404a83296c3a7b847b5199e7ce9773bb3e4
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-09 17:02:22 +0200

    Dodana klasa Racun

---
+++ src/Racun.java
@@ -0,0 +1,13 @@
+public class Racun {
+    public String Naziv;
+    public String KorIme;
+    public String Lozinka;
+    public String Link;
+
+    public Racun(String naziv, String korIme, String lozinka, String link) {
+        Naziv = naziv;
+        KorIme = korIme;
+        Lozinka = lozinka;
+        Link = link;
+    }
+}
