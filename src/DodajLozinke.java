a2ebdcec5576899ad14c540ec13b5ecc7c420dc0
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-09 17:04:19 +0200

    Dodano pozivanje spremanje računa kada upišemo sve podatke

--- src/DodajLozinke.java
+++ src/DodajLozinke.java
@@ -1,5 +1,13 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class DodajLozinke extends JFrame{
     private JPanel panDodaj;
@@ -9,6 +17,8 @@ public class DodajLozinke extends JFrame{
     private JTextField txtLink;
     private JButton btnGenerirajLozinku;
     private JButton btnDodaj;
+    private String korImeKorisnika;
+    private String lozinkaKorisnika;
 
     public DodajLozinke(){
         setTitle("Passwordium");
@@ -25,5 +35,22 @@ public class DodajLozinke extends JFrame{
         btnGenerirajLozinku.setBorderPainted(false);
         btnGenerirajLozinku.setBackground(new Color(200,200,200));
         btnGenerirajLozinku.setFocusPainted(false);
+        btnDodaj.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                KripterPodataka kripterPodataka = new KripterPodataka();
+                Racun racun = new Racun(txtNaziv.getText(),txtKorIme.getText(),new String(txtLozinka.getPassword()),txtLink.getText());
+                try {
+                    kripterPodataka.spremiPodatke(racun,korImeKorisnika,lozinkaKorisnika);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public void podaci(String korIme, String lozinka) {
+        this.korImeKorisnika = korIme;
+        this.lozinkaKorisnika = lozinka;
     }
 }
