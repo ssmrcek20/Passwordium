6210eb9e10c70b3e959d392ceef9a19bb9b137cc
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-05-10 20:46:05 +0200

    Dodana forma za uređivanje računa

---
+++ src/UrediRacun.java
@@ -0,0 +1,105 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+
+public class UrediRacun extends JFrame {
+    private JTextField txtKorIme;
+    private JPasswordField txtLozinka;
+    private JTextField txtNaziv;
+    private JTextField txtLink;
+    private JButton btnGenerirajLozinku;
+    private JButton btnUredi;
+    private JPanel panUredi;
+    private String korImeKorisnika;
+    private String lozinkaKorisnika;
+    private Racun racun;
+    private int odabraniRed;
+
+    public UrediRacun(){
+        setTitle("Passwordium");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(1080,720);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setContentPane(panUredi);
+
+        btnGenerirajLozinku.setBorderPainted(false);
+        btnGenerirajLozinku.setBackground(new Color(200,200,200));
+        btnGenerirajLozinku.setFocusPainted(false);
+
+        btnUredi.setBorderPainted(false);
+        btnUredi.setBackground(new Color(200,200,200));
+        btnUredi.setFocusPainted(false);
+        btnUredi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean ispravno = true;
+
+                if (txtNaziv.getText().equals("")) {
+                    txtNaziv.setBackground(Color.red);
+                    ispravno = false;
+                } else {
+                    txtNaziv.setBackground(Color.white);
+                }
+
+                if (txtKorIme.getText().equals("")) {
+                    txtKorIme.setBackground(Color.red);
+                    ispravno = false;
+                } else {
+                    txtKorIme.setBackground(Color.white);
+                }
+
+                if (txtLozinka.getPassword().length == 0) {
+                    txtLozinka.setBackground(Color.red);
+                    ispravno = false;
+                } else {
+                    txtLozinka.setBackground(Color.white);
+                }
+
+
+
+                if(ispravno) {
+                    KripterPodataka kripterPodataka = new KripterPodataka();
+                    try {
+                        kripterPodataka.izbrisiPodatke(odabraniRed, korImeKorisnika);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(UrediRacun.this, "Greška prilikom uređivanja!");
+                    }
+
+                    Racun noviRacun = new Racun(txtNaziv.getText(),txtKorIme.getText(),new String(txtLozinka.getPassword()),txtLink.getText());
+                    try {
+                        kripterPodataka.spremiPodatke(noviRacun,korImeKorisnika,lozinkaKorisnika);
+                        JOptionPane.showMessageDialog(UrediRacun.this, "Uspješno uređivanje računa!");
+
+                        PrikazSifri prikazSifri= new PrikazSifri();
+                        prikazSifri.podaci(korImeKorisnika, lozinkaKorisnika);
+                        prikazSifri.prikazPodataka();
+                        UrediRacun.this.dispose();
+
+                    } catch (FileAlreadyExistsException ex){
+                        JOptionPane.showMessageDialog(UrediRacun.this,ex.getMessage());
+                    } catch (Exception ex) {
+                        JOptionPane.showMessageDialog(UrediRacun.this, "Došlo je do greške tijekom uređivanja računa!");
+                    }
+                }
+            }
+        });
+    }
+
+    public void prikazPodataka() {
+        txtNaziv.setText(racun.Naziv);
+        txtKorIme.setText(racun.KorIme);
+        txtLink.setText(racun.Link);
+        txtLozinka.setText(racun.Lozinka);
+    }
+
+    public void podaci(String korIme, String lozinka, Racun racun, int odabraniRed) {
+        this.korImeKorisnika = korIme;
+        this.lozinkaKorisnika = lozinka;
+        this.racun = racun;
+        this.odabraniRed = odabraniRed;
+    }
+}
