3f823a267fcd799dabf3149445e66808b5b6e2d9
Filip Šoštarić <fsostaric20@foi.hr>
2023-05-13 18:26:53 +0200

    dodan progressbar koji ukazuje koliko je snazna lozinka

--- src/Registracija.java
+++ src/Registracija.java
@@ -1,14 +1,11 @@
-import javax.security.auth.login.Configuration;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.security.NoSuchAlgorithmException;
 import me.gosimple.nbvcxz.Nbvcxz;
+import me.gosimple.nbvcxz.resources.Configuration;
 import me.gosimple.nbvcxz.resources.ConfigurationBuilder;
 import me.gosimple.nbvcxz.scoring.TimeEstimate;
 
@@ -20,6 +17,9 @@ public class Registracija extends JFrame {
     private JLabel lblPrijava;
     private JLabel lblPotvrdiLozinku;
     private JPasswordField txtPotvrdaLozinke;
+    private JProgressBar PgbJacinaLozinke;
+    private JLabel lblJacinaLozinke;
+    private Nbvcxz nbvcxz;
 
     public Registracija() {
         setTitle("Passwordium");
@@ -32,7 +32,12 @@ public class Registracija extends JFrame {
         btnRegistracija.setBorderPainted(false);
         btnRegistracija.setBackground(new Color(200,200,200));
         btnRegistracija.setFocusPainted(false);
+
+        Configuration configuration = new ConfigurationBuilder().setMinimumEntropy(40d).createConfiguration();
+        nbvcxz = new Nbvcxz(configuration);
         btnRegistracija.addActionListener(new ActionListener() {
+
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 if( provjeriJesuLiPodaciUneseni() ){
@@ -41,31 +46,27 @@ public class Registracija extends JFrame {
 
                     if( potvrdaLozinke() ){
 
-                        if( provjeriJacinuLozinke() ){
-
                             HasherLozinke hasherLozinke= new HasherLozinke();
                             String hashedLozinka = null;
-
-                            try {
-                                hashedLozinka = hasherLozinke.napraviHash(txtKorIme.getText(),new String(txtLozinka.getPassword()));
-
+                            if(provjeriJacinuLozinke()>40d){
                                 try {
-                                    hasherLozinke.spremiLozinku(txtKorIme.getText(),hashedLozinka);
-                                    JOptionPane.showMessageDialog(Registracija.this,"Uspješna registarcija!");
-                                    Registracija.this.dispose();
-                                } catch (FileAlreadyExistsException ex){
-                                    JOptionPane.showMessageDialog(Registracija.this,ex.getMessage());
-                                } catch (IOException ex) {
-                                    JOptionPane.showMessageDialog(Registracija.this,"Došlo je do greške tijekom upisa u datoteku!");
+                                    hashedLozinka = hasherLozinke.napraviHash(txtKorIme.getText(),new String(txtLozinka.getPassword()));
+                                    try {
+                                        hasherLozinke.spremiLozinku(txtKorIme.getText(),hashedLozinka);
+                                        JOptionPane.showMessageDialog(Registracija.this,"Uspješna registracija!");
+                                        Registracija.this.dispose();
+                                    } catch (FileAlreadyExistsException ex){
+                                        JOptionPane.showMessageDialog(Registracija.this,ex.getMessage());
+                                    } catch (IOException ex) {
+                                        JOptionPane.showMessageDialog(Registracija.this,"Došlo je do greške tijekom upisa u datoteku!");
+                                    }
+
+                                } catch (NoSuchAlgorithmException ex) {
+                                    JOptionPane.showMessageDialog(Registracija.this,"Ne postoji SHA-256 na računalu!");
                                 }
-
-                            } catch (NoSuchAlgorithmException ex) {
-                                JOptionPane.showMessageDialog(Registracija.this,"Ne postoji SHA-256 na računalu!");
+                            }else{
+                                JOptionPane.showMessageDialog(Registracija.this,"Upisana lozinka nije dovoljno snažna!");
                             }
-                        }else{
-                            JOptionPane.showMessageDialog(Registracija.this,"Upisana lozinka nije dovoljno snažna!");
-
-                        }
 
                     }else{
                         JOptionPane.showMessageDialog(Registracija.this,"Upisana lozinka se razlikuje od vrijednosti upisane u polje za potvrdu lozinke!");
@@ -80,13 +81,26 @@ public class Registracija extends JFrame {
                 Registracija.this.dispose();
             }
         });
+        txtLozinka.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                double jacinaLozinke = provjeriJacinuLozinke();
+                if(jacinaLozinke<40){
+                    PgbJacinaLozinke.setValue((int)Math.round(jacinaLozinke));
+                    PgbJacinaLozinke.setForeground(Color.red);
+                }else{
+                    PgbJacinaLozinke.setValue(40);
+                    PgbJacinaLozinke.setForeground(Color.green);
+                }
+            }
+        });
     }
 
-    private boolean provjeriJacinuLozinke() {
-        Nbvcxz nbvcxz = new Nbvcxz();
+    private double provjeriJacinuLozinke() {
         String lozinka = new String(txtLozinka.getPassword());
         me.gosimple.nbvcxz.scoring.Result rezultat = nbvcxz.estimate(lozinka);
-        return rezultat.isMinimumEntropyMet();
+        return rezultat.getEntropy();
     }
 
     private boolean potvrdaLozinke(){
